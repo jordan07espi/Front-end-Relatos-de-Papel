9da9ff91fe6d68836468188bfa1b88147a792409
jordan07espi <jeespinosa5@espe.edu.ec>
2024-12-26 18:13:59 -0500

    Landing redirige despues de los 5 segundos

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,13 +1,18 @@
-// src/views/Landing.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import useRedirection from '../hooks/useRedirection';
 import logo from '../assets/hizotipoBlack.png';
 
 function Landing() {
+    useRedirection('/books', 5000); // Redirect to the overview page after 5 seconds
+
     return (
         <div className="main-content">
             <div className="landing">
                 <img src={logo} alt="Logo" />
-                <p>Explora los mejores libros en nuestra tienda virtual.</p>
+                <Link to="/overview">
+                    <p>Explora los mejores libros en nuestra tienda virtual.</p>
+                </Link>
             </div>
         </div>
     );
